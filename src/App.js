4316d55ec6d0b9240a24d823c6f007a697da3dc9
unknown <slodin_741@hotmail.com>
2019-03-05 15:00:21 -0800

    remove some init template comments

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import ProductTable from './components/ProductTable/index';
 import ProductCategorySelect from './components/ProductCategorySelect/index';
 
